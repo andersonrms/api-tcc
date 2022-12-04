ea6ada9c63f8dcc7ba4367e53a69cc8a9dc185db
Anderson Ramos <anderson.ramos@mosaico.com.br>
2022-12-04 00:06:04 -0300

    create products table and product model

---
+++ src/models/Products.js
@@ -0,0 +1,38 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Products extends Model {
+  static init(sequelize) {
+    super.init({
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'name is empty or less than 3',
+          },
+        },
+      },
+      amount: {
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+        validate: {
+          isNumeric: {
+            msg: 'amount does not a number',
+          },
+        },
+      },
+      price: {
+        type: Sequelize.FLOAT,
+        defaultValue: null,
+        validate: {
+          isFloat: {
+            msg: 'price does not a float',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+  }
+}
