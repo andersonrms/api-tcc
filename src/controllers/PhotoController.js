1c78ae85411cd16e98ed0fc8660391537070a843
Anderson Ramos <anderson.ramos@mosaico.com.br>
2023-04-05 20:47:51 -0300

    add secury in photo routes

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,7 +1,17 @@
-class HomeController {
+import multer from 'multer';
+
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('file'); // middleware
+
+class PhotoController {
   async index(req, res) {
-    res.json(req.file);
+    return upload(req, res, (error) => {
+      if (error) return res.status(400).json({ erros: [error.code] });
+
+      return res.json(req.file);
+    });
   }
 }
 
-export default new HomeController();
+export default new PhotoController();
--- src/routes/photoRoutes.js
+++ src/routes/photoRoutes.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import photoController from '../controllers/PhotoController';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig); // middleware
 
 const router = new Router();
 
-router.post('/', upload.single('file'), photoController.index);
+router.post('/', photoController.index);
 
 export default router;
